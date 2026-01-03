ad8911d7040e2cca7728f10808bc812b29ff06da
nayanasyarif <nayanasyarief988@gmail.com>
2026-01-03 14:22:12 +0800

    setup routes & styling

--- fe-perpus/src/main.jsx
+++ fe-perpus/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/home";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  </StrictMode>
+);
